3104bf57c39b92e16ad534e7b71bc3896c76eafa
Oleh Nosul <learnitusa@gmail.com>
2025-02-13 00:48:52 +0100

    started

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -9,14 +9,20 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.PropertyNamingPolicy = null;
 });
 // Initialize Serilog from appsettings.json
-builder.Host.UseSerilog((context, services, configuration) => configuration
-    .ReadFrom.Configuration(context.Configuration)
-    .ReadFrom.Services(services));
+// builder.Host.UseSerilog((context, services, configuration) => configuration
+//     .ReadFrom.Configuration(context.Configuration)
+//     .ReadFrom.Services(services));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<SqlConnectionFactory>();
 builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<RoomTypeRepository>();
+builder.Services.AddScoped<RoomRepository>();
+builder.Services.AddScoped<AmenityRepository>();
+builder.Services.AddScoped<RoomAmenityRepository>();
+
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- HotelBookingAPI/obj/Debug/net8.0/HotelBookingAPI.AssemblyInfo.cs
+++ HotelBookingAPI/obj/Debug/net8.0/HotelBookingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4bcd823aca7c5e711d564bb58f4396c74fa04b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
